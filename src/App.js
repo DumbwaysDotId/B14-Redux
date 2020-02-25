1a6e72145762dffb8079ddfffe7b5c6720f5ada8
radifanmp <radifanmp@gmail.com>
2020-02-25 12:07:22 +0700

    Init Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createStore } from 'redux';
+
+//Make Action
+const actionINC = () => {
+  return {
+    type: 'INCREMENT',
+    payload: null,
+  }
+}
+
+const actionDEC = () => {
+  return {
+    type: 'DECREMENT',
+    payload: null
+  }
+}
+
+
+//Init Reducer
+const initialState = {
+  number: 0,
+}
+
+function counter(state = initialState, action){
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        number: state.number + 1
+      }
+
+      case 'DECREMENT':
+        return {
+          number: state.number - 1
+        }
+      
+      default:
+        return state
+  }
+}
+
+
+//Store
+let store = createStore(counter)
+
+store.subscribe(() => {
+  console.log(store.getState().number)
+})
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App-header  ">
+        <h1>COUNTER</h1>
+        <button onClick={() => store.dispatch(actionINC())}>INC</button>
+        <br></br>
+        <button onClick={() => store.dispatch(actionDEC())}>DEC</button>
+      </div>
+    )
+  }
 }
 
 export default App;
